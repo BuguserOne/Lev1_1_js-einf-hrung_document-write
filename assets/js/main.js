cbe6ef5f2eb213169f9d079967a355b0ef88b939
BuguserOne <jinlingone@gmail.com>
2023-01-04 23:02:55 +0100

    first

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+document.write("<h1>Hello World!</h1>");
+document.write("<p>Have a nice day!</p>")
+
+let myText = "Hello again<br><br>"
+document.write(myText)
+
+let firstName = "Alex"
+let lastName = "Patrin"
+
+document.write("Name: " + firstName + "<br>" + "Nachname: " + lastName);
